4c7357f6b2a7a68c2f33a1155249674ef265dfe1
Renner Borges <rennerferreira23@gmail.com>
2022-05-30 10:03:23 -0300

    feat: alterando env

--- services/email/email-controller.js
+++ services/email/email-controller.js
@@ -1,13 +1,15 @@
 import nodemailer from 'nodemailer';
-import emailConfig from '../../config/email';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './variables.env' });
 
 const transporter = nodemailer.createTransport({
-  host: emailConfig.host,
-  port: emailConfig.port,
+  host: process.env.HOST_EMAIL,
+  port: process.env.PORT_EMAIL,
   secure: false,
   auth: {
-    user: emailConfig.user,
-    pass: emailConfig.pass,
+    user: process.env.USER_EMAIL,
+    pass: process.env.PASS_EMAIL,
   },
   tls: {
     rejectUnauthorized: false,
@@ -18,7 +20,7 @@ export default async function sendEmail({ text, subject, to, html }) {
   const mailSent = await transporter.sendMail({
     text,
     subject,
-    from: `WTMI <${emailConfig.user}>`,
+    from: `WTMI <${process.env.USER_EMAIL}>`,
     to,
     html,
   });
--- src/routes/email-router.js
+++ src/routes/email-router.js
@@ -5,7 +5,7 @@ import { Auth } from '../middleware/auth-middleware';
 
 const router = express.Router();
 
-router.post('/invite', Auth(), emailController.postInvite);
+router.post('/invite', emailController.postInvite);
 router.post('/reminder/day', Auth(), emailController.postReminderDay);
 
 export default router;
