8d9d7485bd88af3a6af998e17304513671d660d4
Renner Borges <rennerferreira23@gmail.com>
2022-05-30 20:19:06 -0300

    feat: alterando swagger.json

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/order */
 const dotenv = require('dotenv');
-const moment = require('moment');
 const swaggerOptions = require('./swagger-autogen.options');
 const swaggerAutogen = require('swagger-autogen')(swaggerOptions);
 
@@ -70,40 +69,6 @@ const doc = {
       SendEmailDefault: {
         email: 'rennerferreira23@gmail.com',
         name: 'Renner',
-      },
-      Login: {
-        email: 'renner@gmail.com',
-        password: '12345678@Re',
-      },
-      CreateCompany: {
-        fantasyName: 'Microsoft',
-        corporateName: 'Microsoft',
-        cnpj: '68035847000145',
-        areaOfOperation: 'Software',
-      },
-      EditCompany: {
-        id: '62804e353f356e900d5eadd9',
-        fantasyName: 'Microsoft',
-        corporateName: 'Microsoft',
-        areaOfOperation: 'Software',
-      },
-      CreateUser: {
-        name: 'Renner Borges',
-        password: '12345678@Re',
-        email: 'rennerferreira23@gmail.com',
-        cpf: '07517373480',
-        tel: '6499295-4946',
-        dateOfBirth: moment().toISOString(),
-        companyId: '628105ab5146f608d0567d6b',
-        role: 'g',
-        workload: 160,
-      },
-      EditUser: {
-        id: '6281096a860c72ada8bb6715',
-        name: 'Renner Borges',
-        tel: '6499295-4946',
-        password: '12345678@Re',
-        workload: 160,
       },
     },
   },
