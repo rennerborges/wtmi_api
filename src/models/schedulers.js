ece460523accb9af5491c42988285551d2ee5fb1
Renner Borges <rennerferreira23@gmail.com>
2022-05-30 08:24:38 -0300

    feat: criando model

---
+++ src/models/schedulers.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const modelSchema = new mongoose.Schema({
+  code: String,
+  title: String,
+  initialDate: Date,
+  finalDate: Date,
+  location: String,
+  vacancyLimit: Number,
+  numberOfSubscribers: Number,
+  pendingRegistrations: Number,
+});
+
+const modelName = 'schedulers';
+
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+  module.exports = mongoose.connection.models[modelName]; // Conexão
+} else {
+  module.exports = mongoose.model(modelName, modelSchema); // Criar nova conexão
+}
