4b08c57c26d9d02eda9ec38f237f84a2bb54dfca
Renner Borges <rennerferreira23@gmail.com>
2022-05-31 13:16:17 -0300

    test: cron 15hrs

--- services/cron/values-cron.js
+++ services/cron/values-cron.js
@@ -1,7 +1,8 @@
 export default {
   EVERY_DAY_00_HOURS: '0 0 * * *',
-  EVERY_DAY_9_HOURS: '0 9 * * *',
   EVERY_DAY_3_HOURS: '0 3 * * *',
+  EVERY_DAY_9_HOURS: '0 9 * * *',
+  EVERY_DAY_15_HOURS: '0 15 * * *',
   EVERY_5_MINUTES: '*/5 * * * *',
   EVERY_15_MINUTES: '*/15 * * * *',
   EVERY_1_MINUTE: '*/1 * * * *',
